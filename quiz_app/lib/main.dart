f6640d1049fb91ff4f896185e8cd7f1152ca1d2f
Mert Aksoy <mrtaksy99@gmail.com>
2023-11-02 22:55:08 +0300

    update

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -22,22 +22,16 @@ class MyApp extends StatelessWidget {
 
 class Anasayfa extends StatefulWidget {
   const Anasayfa({super.key});
-
-
-
   @override
   State<Anasayfa> createState() => _AnasayfaState();
 }
 
 class _AnasayfaState extends State<Anasayfa> {
-
-
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
       appBar: AppBar(
-
         title:const  Text("Ana Sayfa"),
       ),
       body: Center(
